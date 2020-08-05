beb5718b5f747a26d4e55e914a9779a0f892b5b4
Hollis Akins <akinshol@grinnell.edu>
2020-08-05 18:59:37 -0500

    image updates

--- Analysis/Images/images.py
+++ Analysis/Images/images.py
@@ -36,10 +36,10 @@ def read_file(simname):
 
 
     
-fig = plt.figure(dpi=300, figsize=(7.5, 5.8), constrained_layout=True)
+fig = plt.figure(dpi=300, figsize=(7.5, 5.8))
 
 gs = mpl.gridspec.GridSpec(3,3, height_ratios=[1,1,0.05], figure=fig)
-gs.update(hspace=0.03, wspace=0.03)
+gs.update(hspace=0.05, wspace=0.05, right=0.99, top=0.99, left=0.01, bottom=0.08)
 
 # top row, Sonia, bottom row, Sandra
 
