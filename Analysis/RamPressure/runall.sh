1a242187265ad44a45f4002bc26a92c99073813d
Hollis Akins <akinshol@grinnell.edu>
2020-11-09 22:13:26 -0600

    runall

--- Analysis/RamPressure/runall.sh
+++ Analysis/RamPressure/runall.sh
@@ -4,10 +4,18 @@ cd ~/Research/Justice_League_Code/Analysis/RamPressure/
 pwd
 date
 
-python particletracking.py h329 11 & 
-python particletracking.py h329 31 & 
-...
-# you get the picture
+python particletracking.py h148 13 & 
+python particletracking.py h229 20 &
+python particletracking.py h242 24 & 
+wait
+python particletracking.py h148 28 & 
+python particletracking.py h229 22 & 
+python particletracking.py h242 80 &
+wait
+python particletracking.py h148 37 & 
+wait 
+python particletracking.py h148 68 & 
+wait
 
 # include `wait` in between commands to do them in batches
 # by my estimate each command uses at most 5-7% of the memory on quirm, so don't run more than 10 at once (ideally less than 10)
