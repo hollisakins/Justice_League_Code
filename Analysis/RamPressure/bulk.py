baf76d3ef45e7205fb1f8e88ea54f0f71a8fab96
Hollis Akins <hollis.akins@gmail.com>
2021-02-24 14:00:34 -0700

    updated bulk.phy

--- Analysis/RamPressure/bulk.py
+++ Analysis/RamPressure/bulk.py
@@ -7,64 +7,92 @@ import numpy as np
 import pandas as pd
 
 
-def read_tracked_particles(sim, haloid):
+def get_keys():
+    path = '../../Data/tracked_particles.hdf5'
+    with pd.HDFStore(path) as hdf:
+        keys = [k[1:] for k in hdf.keys()]
+    print(*keys)
+    return keys
+
+
+def read_tracked_particles(sim, haloid, verbose=False):
+    
+    if verbose: print(f'Loading tracked particles for {sim}-{haloid}...')
     
     key = f'{sim}_{str(int(haloid))}'
 
     # import the tracked particles dataset
     path = '../../Data/tracked_particles.hdf5'
-    particles = pd.read_hdf(path, key=key)
-
-    # import the ram pressure dataset 
-    path = '../../Data/ram_pressure.hdf5'
-    properties = pd.read_hdf(path, key=key)
-
-    # from the RP dataset, isolate the baryonic disk mass as a func. of time
-    M = np.array(properties.M_star,dtype=float) + np.array(properties.M_gas,dtype=float)
-    t = np.array(properties.t, dtype=float)
-    masses = pd.DataFrame({'time':t, 'M':M})
-
-    # merge the two together on a left join so that we get the masses with the particles
-    data = pd.merge(particles, masses, on='time', how='left')
-
-    # calculate kinetic energy of each particle
-    m = np.array(data.mass) * 1.989e30
-    v = np.array(data.v) * 1000
-    K = 0.5 * m * v * v
-
-    # and potential energy
-    M = np.array(data.M) * 1.989e30
-    r = np.array(data.r) * 3.086e19
-    U = 6.6743e-11 * M * m / r
-
-    ratio = K/U
-
-    data['K'] = K
-    data['U'] = U
-    data['ratio'] = ratio
-
-    # remove the simple classifications from the dataset, to replace with better ones
-    data = data.drop(columns=['sat_disk', 'sat_halo', 'IGM', 'host_halo', 'host_disk', 'classification',])
+    data = pd.read_hdf(path, key=key)
     
-    thermo_disk = (np.array(data.temp) < 1.2e4) & (np.array(data.rho) > 0.1) # thermodynamic disk
-    grav_disk = np.array(data.ratio) < 1 # graviationally bound disk 
-    in_satellite = np.array(data.r_per_Rvir) < 1 
+    if verbose: print('Successfully loaded')
+    
+    r_gal = np.array([])
+    for t in np.unique(data.time):
+        d = data[data.time==t]
+        r_gas = np.mean(d.sat_r_gas)
+        r_half = np.mean(d.sat_r_half)
+        rg = np.max([r_gas,r_half])
 
-    cool_disk = thermo_disk & grav_disk & in_satellite
-    hot_disk = ~thermo_disk & grav_disk & in_satellite
-    cool_halo = thermo_disk & ~grav_disk & in_satellite
-    hot_halo = ~thermo_disk & ~grav_disk & in_satellite
-    CGM = ~in_satellite
-    c = cool_disk + 2*hot_disk + 3*cool_halo + 4*hot_halo + 5*CGM
+        if np.isnan(rg):
+            rg = r_gal_prev
 
-    data['cool_disk'] = cool_disk
-    data['hot_disk'] = hot_disk
-    data['cool_halo'] = cool_halo
-    data['hot_halo'] = hot_halo
-    data['CGM'] = CGM
-    data['c'] = c
+        if verbose: print(f't = {t:1f} Gyr, satellite R_gal = {rg:.2f} kpc')
+        r_gal = np.append(r_gal,[rg]*len(d))
 
+        r_gal_prev = rg
 
+    data['r_gal'] = r_gal
+    
+    r_gal_prev = 0
+    r_gal = np.array([])
+    for t in np.unique(data.time):
+        d = data[data.time==t]
+        r_gas = np.mean(d.host_r_gas)
+        r_half = np.mean(d.host_r_half)
+        rg = np.max([r_gas,r_half])
+
+        if np.isnan(rg):
+            rg = r_gal_prev
+
+        if verbose: print(f't = {t:1f} Gyr, host R_gal = {rg:.2f} kpc')
+        r_gal = np.append(r_gal,[rg]*len(d))
+
+        r_gal_prev = rg
+
+    data['host_r_gal'] = r_gal
+    
+    thermo_disk = (np.array(data.temp) < 1.2e4) & (np.array(data.rho) > 0.1)
+    
+    in_sat = np.array(data.in_sat)
+    sat_disk = in_sat & (np.array(data.r) <= np.array(data.r_gal))
+    sat_halo = in_sat & (np.array(data.r) > np.array(data.r_gal))
+    sat_cool_disk = sat_disk & thermo_disk
+    sat_hot_disk = sat_disk & ~thermo_disk
+    sat_cool_halo = sat_halo & thermo_disk
+    sat_hot_halo = sat_halo & ~thermo_disk
+
+    in_host = np.array(data.in_host) & ~in_sat
+    host_disk = in_host & (np.array(data.r_rel_host) <= np.array(data.host_r_gal))
+    host_halo = in_host & (np.array(data.r_rel_host) > np.array(data.host_r_gal))
+
+    other_sat = np.array(data.in_other_sat)
+    IGM = np.array(data.in_IGM)
+    
+    
+    # basic classifications
+    data['sat_disk'] = sat_disk
+    data['sat_halo'] = sat_halo
+    data['host_disk'] = host_disk
+    data['host_halo'] = host_halo
+    data['other_sat'] = other_sat
+    data['IGM'] = IGM
+    
+    # more advanced classifications
+    data['cool_disk'] = sat_cool_disk
+    data['hot_disk'] = sat_hot_disk
+    data['cool_halo'] = sat_cool_halo
+    data['hot_halo'] = sat_hot_halo
 
     return data
 
@@ -86,10 +114,11 @@ def calc_angles(d):
         
     return d
 
-def calc_ejected_expelled(sim, haloid):
+def calc_ejected_expelled(sim, haloid, save=True, verbose=True):
     import tqdm
-    data = read_tracked_particles(sim, haloid)
+    data = read_tracked_particles(sim, haloid, verbose=verbose)
 
+    if verbose: print(f'Now computing ejected/expelled particles for {sim}-{haloid}...')
     ejected = pd.DataFrame()
     expelled = pd.DataFrame()
 
@@ -101,7 +130,12 @@ def calc_ejected_expelled(sim, haloid):
         hot_disk = np.array(dat.hot_disk, dtype=bool)
         cool_halo = np.array(dat.cool_halo, dtype=bool)
         hot_halo = np.array(dat.hot_halo, dtype=bool)
-        CGM = np.array(dat.CGM, dtype=bool)
+
+        host_halo = np.array(dat.host_halo, dtype=bool)
+        host_disk = np.array(dat.host_disk, dtype=bool)
+        IGM = np.array(dat.IGM, dtype=bool)
+        other_sat = np.array(dat.other_sat, dtype=bool)
+        
         time = np.array(dat.time,dtype=float)
         
         can_be_expelled = False
@@ -124,8 +158,21 @@ def calc_ejected_expelled(sim, haloid):
                     can_be_expelled = True
                 
                 if can_be_expelled:
-                    if (cool_halo[i-1] or hot_halo[i-1]) and CGM[i]:    
-                        expelled = pd.concat([expelled, dat[time==t2]])
+                    if (cool_halo[i-1] or hot_halo[i-1]) and host_halo[i]:    
+                        state2 = 'host halo'
+                        out = dat[time==t2].copy()
+                        out['state2'] = state2
+                        expelled = pd.concat([expelled, out])
+                    if (cool_halo[i-1] or hot_halo[i-1]) and host_disk[i]:    
+                        state2 = 'host disk'
+                        out = dat[time==t2].copy()
+                        out['state2'] = state2
+                        expelled = pd.concat([expelled, out])
+                    if (cool_halo[i-1] or hot_halo[i-1]) and IGM[i]:    
+                        state2 = 'IGM'
+                        out = dat[time==t2].copy()
+                        out['state2'] = state2
+                        expelled = pd.concat([expelled, out])
 
 
     # apply the calc_angles function along the rows of ejected and expelled
@@ -133,7 +180,38 @@ def calc_ejected_expelled(sim, haloid):
     ejected = ejected.apply(calc_angles, axis=1)
     print('Calculating expulsion angles')
     expelled = expelled.apply(calc_angles, axis=1)
+    
+    if save:
+        key = f'{sim}_{str(int(haloid))}'
+        filepath = '../../Data/ejected_particles.hdf5'
+        print(f'Saving {key} ejected particle dataset to {filepath}')
+        ejected.to_hdf(filepath, key=key)
+
+        filepath = '../../Data/expelled_particles.hdf5'
+        print(f'Saving {key} expelled particle dataset to {filepath}')
+        expelled.to_hdf(filepath, key=key)
+        
+        
+    print(f'Returning both datasets...')
 
     return ejected, expelled
         
-                
+
+def read_ejected_expelled(sim, haloid):
+    key = f'{sim}_{str(int(haloid))}'
+    ejected = pd.read_hdf('../../Data/ejected_particles.hdf5', key=key)
+    expelled = pd.read_hdf('../../Data/expelled_particles.hdf5', key=key)
+    print(f'Returning ejected,expelled for {sim}-{haloid}...')
+    return ejected, expelled
+        
+    
+def read_all_ejected_expelled():
+    ejected = pd.DataFrame()
+    expelled = pd.DataFrame()
+    keys = get_keys()
+    for key in keys:
+        ejected = pd.concat([ejected, pd.read_hdf('../../Data/ejected_particles.hdf5', key=key)])
+        expelled = pd.concat([expelled, pd.read_hdf('../../Data/expelled_particles.hdf5', key=key)])
+    print(f'Returning ejected,expelled for all available satellites...')
+    return ejected, expelled
+
