ba9e0760cc7a4637213840a2e3a0c142fb4027a1
Hollis Akins <hollis.akins@gmail.com>
2021-04-15 10:16:05 -0600

    update write ejected expelled

--- Analysis/RamPressure/write_ejected_expelled.py
+++ Analysis/RamPressure/write_ejected_expelled.py
@@ -6,4 +6,4 @@ keys = get_keys()
 for key in keys:
     sim = str(key[:4])
     haloid = int(key[5:])
-    ejected, expelled, accreted = calc_ejected_expelled_accreted(sim,haloid, verbose=True, save=True)
+    ejected, cooled, expelled, accreted = calc_ejected_expelled(sim,haloid, verbose=True, save=True)
