0cf6a24d3342e0b58485a23af56f6abcdedc281c
Hollis Akins <hollis.akins@gmail.com>
2022-04-10 20:19:36 -0500

    notebooks Apr 2022

--- Analysis/RamPressure/write_ejected_expelled.py
+++ Analysis/RamPressure/write_ejected_expelled.py
@@ -1,12 +1,11 @@
 from analysis import *
 
-keys = ['h148_13','h148_28','h148_37','h148_45','h148_68','h148_80','h148_283',
-        'h148_278','h148_329','h229_20','h229_22','h229_23','h229_27','h229_55',
-        'h242_24','h242_41','h242_80','h329_33','h329_137']
+keys = ['h148_12','h148_249','h148_251','h148_27','h148_282','h148_34','h148_38','h148_55','h148_65','h229_14',
+        'h229_18','h229_20','h229_22','h229_49','h242_21','h242_38','h242_69','h329_117','h329_29']
 print(keys)
 print(len(keys))
 
 for key in keys:
     sim = str(key[:4])
     haloid = int(key[5:])
-    ejected, cooled, expelled, accreted = calc_ejected_expelled(sim, haloid, save=True, verbose=False)
+    expelled, accreted = calc_ejected_expelled(sim, haloid, save=True, verbose=False)
