25d0f3fb52e9cd1d5dbb70d5354c51d6534feb11
Hollis Akins <hollis.akins@gmail.com>
2022-05-10 15:13:48 -0500

    run all log file

--- Analysis/RamPressure/runall.sh
+++ Analysis/RamPressure/runall.sh
@@ -5,31 +5,46 @@ pwd
 date
 
 # updated to use new haloids Dec 31 2022
+# python particletracking.py h242 38 & # 1
+# python particletracking.py h329 29 & # 2
+# python particletracking.py h229 22 & # 3
+# python particletracking.py h148 12 & # 4
+# wait
+# python particletracking.py h229 14 & # 5
+# python particletracking.py h242 21 & # 6
+# python particletracking.py h229 18 & # 7
+# python particletracking.py h242 69 & # 8
+# wait
+# python particletracking.py h148 34 & # 9
+# python particletracking.py h148 27 & # 10
+# python particletracking.py h148 55 & # 11
+# python particletracking.py h148 251 & # 12
+# wait
+# python particletracking.py h229 20 & # 13
+# python particletracking.py h148 38 & # 14
+# python particletracking.py h148 249 & # 15
+# python particletracking.py h229 49 & # 16
+# wait
+# python particletracking.py h329 117 & # 17
+# python particletracking.py h148 65 & # 18
+# python particletracking.py h148 282 & # 19
+# wait
 
-python particletracking.py h242 38 & # 1
-python particletracking.py h329 29 & # 2
-python particletracking.py h229 22 & # 3
-python particletracking.py h148 12 & # 4
+python particletracking.py h148 2 > logs/h148_2.log &
+python particletracking.py h242 30 > logs/h242_30.log &
+python particletracking.py h148 6 > logs/h148_6.log &
+python particletracking.py h329 7 > logs/h329_7.log &
+wait 
+python particletracking.py h148 3 > logs/h148_3.log &
+python particletracking.py h148 7 > logs/h148_7.log &
+python particletracking.py h242 401 > logs/h242_401.log &
+python particletracking.py h242 8 > logs/h242_8.log &
+wait 
+python particletracking.py h148 4 > logs/h148_4.log &
+python particletracking.py h148 10 > logs/h148_10.log &
+python particletracking.py h242 10 > logs/h242_10.log &
 wait
-python particletracking.py h229 14 & # 5
-python particletracking.py h242 21 & # 6
-python particletracking.py h229 18 & # 7
-python particletracking.py h242 69 & # 8
-wait
-python particletracking.py h148 34 & # 9
-python particletracking.py h148 27 & # 10
-python particletracking.py h148 55 & # 11
-python particletracking.py h148 251 & # 12
-wait
-python particletracking.py h229 20 & # 13
-python particletracking.py h148 38 & # 14
-python particletracking.py h148 249 & # 15
-python particletracking.py h229 49 & # 16
-wait
-python particletracking.py h329 117 & # 17
-python particletracking.py h148 65 & # 18
-python particletracking.py h148 282 & # 19
-wait
+
 
 # include `wait` in between commands to do them in batches
 # by my estimate each command uses at most 5-7% of the memory on quirm, so don't run more than 10 at once (ideally less than 10)
